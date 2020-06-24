f10530694bf3423cd5d48d1a6a0c72c3e4a76430
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-06-24 10:28:15 +0530

    Finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+require("dotenv").config();
+require("./models/User");
+require("./services/passport");
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const authRoutes = require("./routes/authRoutes");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 8000;
 
