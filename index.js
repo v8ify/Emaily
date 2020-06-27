2480cfd93f6e92c4964a7c354607dfe40b38bcef
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-06-27 10:07:46 +0530

    Added heroku build process

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
