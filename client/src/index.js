bca64e076fe820dca00629e4dc584b1f529cc396
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-06-28 11:43:29 +0530

    Completed sendgrid setup on server side

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import axios from "axios";
 
 import App from "./components/App";
 import reducers from "./reducers";
+
+window.axios = axios;
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
